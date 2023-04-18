5104723842b827108001ca53b69a806c3e3b8b05
Huy Tyskland <nguyenhuy199777@gmail.com>
2023-04-18 15:12:57 +0200

    set up service for non platoon vehicles

---
+++ scenarios/step1_multilane_noConsideration/NPService.cc
@@ -0,0 +1,51 @@
+#include "NPService.h"
+#include "msgs/V2Vmsg_m.h"
+#include "artery/traci/VehicleController.h"
+#include <vanetza/btp/data_request.hpp>
+#include <vanetza/dcc/profile.hpp>
+#include <vanetza/geonet/interface.hpp>
+
+using namespace omnetpp;
+using namespace vanetza;
+
+Define_Module(NPService)
+
+void NPService::initialize()
+{
+    ItsG5Service::initialize();
+    mVehicleController = &getFacilities().get_const<traci::VehicleController>();
+    NPInfo.ID = mVehicleController->getVehicleId();
+    NPInfo.lane = mVehicleController->getLaneIndex(NPInfo.ID);
+}
+
+void NPService::trigger()
+{
+    Enter_Method("NPService trigger");
+    auto& vehicle_api = mVehicleController->getLiteAPI().vehicle();
+
+    auto msg = new V2Vmsg();
+    msg->setID(NPInfo.ID);
+    msg->setLane(NPInfo.lane);
+    msg->setIsPM(0);
+    msg->setByteLength(40);
+
+    btp::DataRequestB req;
+    req.destination_port = host_cast<NPService::port_type>(getPortNumber());
+    re.gn.transport_type = geonet::TransportType::SHB;
+    req.gn.traffic_class.tc_id(static_cast<unsigned>(dcc::Profile::DP0));
+    req.gn.communication_profile = geonet::CommunicationProfile::ITS_G5;
+    request(req, msg);
+}
+
+void NPService::indicate(const vanetza::btp::DataIndication& ind, omnetpp::cPacket* packet)
+{
+    Enter_Method("NPService indicate");
+    auto msg = check_and_cast<const V2Vmsg*>(packet);
+
+    auto& vehicle_api = mVehicleController->getLiteAPI().vehicle();
+    if (msg->getIsPM() == 1 && (msg->getID())[0] == 'p') // messages from member in the same lane
+    {
+
+    }
+    delete msg;
+}
---
+++ scenarios/step1_multilane_noConsideration/NPService.h
@@ -0,0 +1,22 @@
+#ifndef NPSERVICE_H_
+#define NPSERVICE_H_
+
+#include "artery/application/ItsG5Service.h"
+#include "vehicle.h"
+
+// forward declaration
+namespace traci { class VehicleController; }
+
+class NPService : public artery::ItsG5Service
+{
+    public:
+        void trigger() override;
+
+    protected:
+        void initialize() override;
+        void indicate(const vanetza::btp::DataIndication&, omnetpp::cPacket*)
+        const traci::VehicleController* mVehicleController = nullptr;
+        vehicle NPInfo;
+};
+
+#endif /* NPSERVICE_H_ */
